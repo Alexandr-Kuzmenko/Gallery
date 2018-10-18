31f45b29ee4be66216f68d4b5c0a389a300c67b6
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-10-18 11:34:28 +0300

    working link

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   resources :wallpapers do
     resources :posts
   end
-
+  get 'wallpapers/:id/fullsize', to: 'wallpapers#fullsize', as: 'fullsize', method: 'show'
   root 'categories#index'
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get 'wallpapers/:id/fullsize', to: 'wallpapers#fullsize', as: 'fullsize', method: 'show'
+  
 end
