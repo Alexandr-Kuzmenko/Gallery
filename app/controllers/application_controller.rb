c7797092c7a2d54f7703e66a20c10cdbe0fe5a7b
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-10-18 12:58:50 +0300

    common rights for unlogged guests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  protect_from_forgery except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show] 
 
-  def after_sign_in_path_for(_resource)
-    wallpapers_path
-  end
+  #def after_sign_in_path_for(_resource)
+  #  wallpapers_path
+  #end
 
-  def after_sign_out_path_for(resource_or_scope)
-    request.referrer
-  end
+  #def after_sign_out_path_for(resource_or_scope)
+  #  request.referrer
+  #end
 end
