718d62864510183aa49827d93c9a696de125471f
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-10-26 21:04:01 +0300

    model small fixes

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,11 +1,15 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :avatar, ImageUploader
   validates :email, :password, presence: true
+  validates :email, :nickname, uniqueness: true
   validates :password, confirmation: true, length: { in: 6..20 }
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
   after_create { |admin| admin.send_reset_password_instructions }
+
   def password_required?
     new_record? ? false : super
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :avatar, ImageUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, :password, presence: true
   validates :password, confirmation: true, length: { in: 6..20 }
+  validates :email, :nickname, uniqueness: true
   has_many :categories
   has_many :comments
 end
