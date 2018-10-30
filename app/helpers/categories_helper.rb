576aa4e42a16eded0a42f4f3abdb35bb6a8a1a43
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-10-30 18:49:40 +0200

    helper fixed due to last changes

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,9 +1,9 @@
 module CategoriesHelper
   def own?(obj) 
     if current_admin_user
-      return if current_admin_user == obj.admin_user
+      return if current_admin_user == obj.categorized && obj.categorized_type == 'AdminUser'
     elsif current_user
-      return if current_user == obj.user
+      return if current_user == obj.categorized && obj.categorized_type == 'User'
     else
       false
     end
