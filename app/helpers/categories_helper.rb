fa3d48812d1d6abc7c0c066da76e0df9c604ddda
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-10-30 18:30:34 +0200

    helper of parent ident. changed due to mutual changes

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,6 +1,12 @@
 module CategoriesHelper
   def own?(obj) 
-    current_user.id == obj.user_id if current_user.present?
+    if current_admin_user
+      return if current_admin_user == obj.admin_user
+    elsif current_user
+      return if current_user == obj.user
+    else
+      false
+    end
   end
 end
 
