cef71d4812cb20dc69aa4a83095a9656b8b069e1
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-11-02 15:40:28 +0200

    made db/seeds.rb according to requirements to autofill the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def random_likes_create
+  wp_count = Wallpaper.all.size
+  if Wallpaper.first && User.first
+    id = User.first.id
+    10.times do
+      wid = rand(wp_count) + 1
+      Like.create!(user_id: id, wallpaper_id: wid) unless Like.find_by wallpaper_id: wid, user_id: id
+    end
+  end
+end
+
+def random_commnets_create
+  wp_count = Wallpaper.all.size
+  if Wallpaper.first && User.first
+    user = User.first
+    3.times do
+      wid = rand(wp_count) + 1
+      user.comments.create!(wallpaper_id: wid, text: 'random comment') unless user.comments.find_by wallpaper_id: wid
+    end
+  end
+
+end
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 User.create!(email: 'user@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Rake::Task["autofilling:init_filling"].execute
+random_likes_create
+random_commnets_create
