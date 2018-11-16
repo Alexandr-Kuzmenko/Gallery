acb4509c58ec157f3578a89e9c4b917d6a61dae3
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-11-16 14:42:19 +0200

    routes optimized

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,33 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|ru/ do
+  scope '(:locale)', locale: /en|ru/ do
     devise_for :admin_users, :categories, :wallpapers, :comments, ActiveAdmin::Devise.config
     ActiveAdmin.routes(self)
 
     devise_for :users
     resources :users, :comments, :activities
+
     resources :wallpapers do
-      resources :comments, :likes
+      resources :comments
+      resources :likes, only: [:new, :create, :destroy]
     end
+
     resources :categories do
-      resources :subscriptions
+      resources :subscriptions, only: [:new, :create, :destroy]
     end
+
+    get ':/locale', to: 'categories#index'
     get 'user/index', to: 'users#index'
     get 'wallpaper/:id/fullsize', to: 'wallpapers#fullsize', as: 'fullsize', method: 'show'
+
     match 'admin/parsing', to: 'admin/parsing#index', via: [:get, :post]
-    #resources :users do
-    #  member do
-    #    get 'user/index', to: 'users#index'
-    #  end
-    #end
-    get ':/locale', to: 'categories#index'
+
     root 'categories#index'
+
+    # resources :users do
+    #   member do
+    #     get 'user/index', to: 'users#index'
+    #   end
+    # end
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 end
