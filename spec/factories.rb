d228266cd4cc0ea9f8ed4443f7905f585d702388
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-11-26 19:19:50 +0200

    proper settings for using FactoryBot while creating models

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,19 +1,38 @@
+# require 'faker'
 FactoryBot.define do
   factory :admin_user, class: AdminUser do
-    email 'some_admin_email_001@example.com'
-    password 'password'
-    password_confirmation 'password'
-    nickname 'spec_admin'
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(8) }
+  #  password_confirmation { Faker::Internet.password(8) }
+    nickname { Faker::Internet.username(8) }
   end
-  factory :user, class: User do
-    email 'some_email_001@example.com'
-    password 'password'
-    password_confirmation 'password'
-    nickname 'spec_user'
-  end
-  factory :category, class: Category do
-    name 'spec_category'
-    locked false
-    association :categorized, factory: [:user, :admin_user]
-  end
+  # factory :user, class: User do
+  #   email 'some_email_001@example.com'
+  #   password 'password'
+  #   password_confirmation 'password'
+  #   nickname 'spec_user'
+  # end
+  # factory :category, class: Category do
+  #   name 'spec_category'
+  #   locked false
+  #   association :categorized, factory: [:user, :admin_user]
+  # end
 end
+
+#  example_below
+#  spec/factories/students.rb
+#  FactoryGirl.define do
+#    factory :student do
+#      full_name     { Faker::Name.name }
+#      age           { Faker::Number.between(18, 65) }
+#      email         { Faker::Internet.email}
+#      phone_number  { Faker::PhoneNumber.phone_number }
+#      university
+#    end
+#  end
+#  spec/factories/universities.rb
+#  FactoryGirl.define do
+#    factory :university do
+#      name     { Faker::University.name }
+#    end
+#  end
---
+++ spec/manual/first_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe AdminUser, type: :model do
+  before(:all) do
+    @user = FactoryBot.create(:admin_user)
+  end
+
+
+#  before do
+#    @user = FactoryGirl.create(email: 'user@example.com', password: 'password', password_confirmation: 'password')
+#    User.create!(email: 'user@example.com', password: 'password', password_confirmation: 'password')
+#  end
+#
+#  subject { @user }
+#
+#  describe "check fields" do
+#    it { expect(@user).to respond_to(:email) }
+#  #  it { expect(@user).to respond_to(:description) }
+#  #  it { expect(@user).to respond_to(:pictures) }
+#  #  it { expect(@user).to respond_to(:user_categories) }
+#  #  it { expect(@user).to respond_to(:users) }
+#  end
+  describe 'name' do
+    it 'Random admin_user creating' do
+      # @user = FactoryBot.create(admin_user)
+      # @name = Faker::Name.name
+      puts "current users email is: #{@user.email}, password: #{@user.encrypted_password}, nickname: #{@user.nickname}"
+      expect(@user.email).not_to be_nil
+    end
+  end
+# describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "get first user" do
+  #   it "returns first user" do
+  #     user = User.first
+  #     expect(user).to be nil
+  #   end
+  # end
+
+  # describe "User"
+  #   describe ".top" do
+  #
+  #     before { FactoryGirl.create_list(:user, 3) }
+  #     it { User.top(2).should have(2).item }
+  #   end
+  # end
+
+
+
+  # describe 'some user' do
+  #   it 'may be created' do
+  #     @user = FactoryBot.create(:user)
+  #     pust
+  #     expect(@user.email).to be != nil
+  #   end
+  # end
+end
