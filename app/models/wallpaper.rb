b44538efef7cdb44115ecde9173f8ea16318f212
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-11-26 19:20:30 +0200

    model fix to have a counter

--- app/models/wallpaper.rb
+++ app/models/wallpaper.rb
@@ -1,6 +1,6 @@
 class Wallpaper < ApplicationRecord
   mount_uploader :image, ImageUploader
-  belongs_to :category
+  belongs_to :category, counter_cache: true
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   extend FriendlyId
