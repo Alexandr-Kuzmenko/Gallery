44a65612c9a72c1cd9e1dd7a442b435ead7f6840
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2019-01-02 18:35:57 +0200

    testing real configs ot sending mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,26 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test001_#{Rails.env}"
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'obscure-anchorage-48036.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      user_name: 'admin@example.com',
+      password: 'password',
+
+      # user_name: ENV['DEV_MAILER_USER'],
+      # password: ENV['DEV_MAILER_PASS'],
+
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
