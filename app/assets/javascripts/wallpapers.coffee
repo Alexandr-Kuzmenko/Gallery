98169cd25254af7f02c505e11d8ca17dfa93143d
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2019-01-04 15:40:37 +0200

    functionality fixes

--- app/assets/javascripts/wallpapers.coffee
+++ app/assets/javascripts/wallpapers.coffee
@@ -1,3 +1,4 @@
-$(document).ready ->
+$(document).on 'turbolinks:load', ->
+  $(document).ready ->
   $('print_field_btn').click ->
     $('.new_comment_block').toggle()
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,9 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6Ldic3kUAAAAAOjUG3oaq3qvpD9f05G_oSy8zmwO'
-  config.secret_key = '6Ldic3kUAAAAAB06X_42pEp_veOVlsLvROQSOrHh'
+  # local
+  config.site_key  = '6Ldic3kUAAAAAOjUG3oaq3qvpD9f05G_oSy8zmwO' if Rails.env.development?
+  config.secret_key = '6Ldic3kUAAAAAB06X_42pEp_veOVlsLvROQSOrHh' if Rails.env.development?
+  # prod on heroku
+  config.site_key = '6LcesIYUAAAAAL_Y4heytptdTn21sqrjLqoKMEND' if Rails.env.production?
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
